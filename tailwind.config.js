81e536ad6c9d43277ed3328549ba658494d4dc41
Ritesh Yadav <yadavritesh2899@gmailcom>
2021-06-29 14:09:29 +0530

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  mode: "jit",
+  purge: ["./index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
